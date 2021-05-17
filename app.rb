7269c9d8cda4b990b7b122374006b0a8d4d13421
Muhammad <muhammadhullah23@gmail.com>
2021-05-17 18:09:35 -0400

    commit tree 189b20c6f74725db10acae3d6b9ed232b97bef76

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Hammad."
+    end
+
+    get '/hometown' do
+        "My hometown is Jersey City."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is The Sounds of Silence."
+    end
+
 end
